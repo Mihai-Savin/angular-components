810a66ef6955d8d333324f79406c71c043ab83ab
socaciuvlad <vladsocaciucatalin@gmail.com>
2024-04-03 13:45:33 +0300

    generated new box-bar component

---
+++ projects/ngxw/widgets/src/lib/box-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-box-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-bar.component.html',
+  styleUrl: './box-bar.component.css'
+})
+export class BoxBarComponent {
+
+}
